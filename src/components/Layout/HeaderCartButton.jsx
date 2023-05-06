00f1a94954f0af17984e8de0457a47c72aa70ba3
jeanchaneto <92741347+jeanchaneto@users.noreply.github.com>
2023-05-06 14:15:01 +0200

    Cart header btn animation with useEffect.

--- src/components/Layout/HeaderCartButton.jsx
+++ src/components/Layout/HeaderCartButton.jsx
@@ -1,9 +1,11 @@
-import { useContext } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import CartIcon from '../Cart/CartIcon'
 import classes from './HeaderCartButton.module.css'
 import CartContext from '../../store/cart-context'
 
 const HeaderCartButton = (props) => {
+
+  const [btnIsAnimated, setBtnIsAnimated ] =  useState(false)
 
   const cartCtx = useContext(CartContext);
 
@@ -11,8 +13,26 @@ const HeaderCartButton = (props) => {
     return currentNumber + item.amount
   }, 0)
 
+const btnClasses = `${classes.button} ${btnIsAnimated ? classes.bump : ''}`;
+ 
+useEffect(() => {
+  if (cartCtx.items.length === 0) {
+    return;
+  }
+  setBtnIsAnimated(true);
+
+const timer = setTimeout(() => {
+  setBtnIsAnimated(false);
+}, 300);
+
+return () => {
+  clearTimeout(timer);
+}
+
+}, [cartCtx.items])
+
   return (
-    <button className={classes.button} onClick={props.onClick} >
+    <button className={btnClasses} onClick={props.onClick} >
       <span>
       <CartIcon />
       </span>
